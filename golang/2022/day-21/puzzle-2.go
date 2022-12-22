1bbcf2e5c53e0b3d4311ed129ca78aaebfbfa072
Toni Tienda <tonitienda@gmail.com>
2022-12-22 18:48:19 +0900

    golang: 2022 - day 21 - starting with solving the equation

--- golang/2022/day-21/puzzle-2.go
+++ golang/2022/day-21/puzzle-2.go
@@ -68,7 +68,7 @@ func makeMonkeyMap(monkeys []Monkey) map[string]Monkey {
 
 func main() {
 
-	execType := "test"
+	execType := "input"
 	data := input.GetLines(2022, 21, execType+".txt")
 
 	monkeys := array.Map(data, getMonkey)
@@ -80,12 +80,69 @@ func main() {
 	rootMonkey.operation = "="
 	index["root"] = rootMonkey
 
+	humnMonkey := index["humn"]
+	humnMonkey.isSolved = false
+	index["humn"] = humnMonkey
+
 	index2 := map[string]func() int{}
 	index3 := map[string]bool{}
 
 	descriptions := map[string]string{}
 
 	currentHumnValue := 0
+	somethingSolved := true
+
+	// Solve everythink solvable
+	for somethingSolved {
+		somethingSolved = false
+
+		for key, monkey := range index {
+			if key != "humn" && key != "root" && !monkey.isSolved {
+				m1 := index[monkey.m1]
+				m2 := index[monkey.m2]
+
+				if m1.isSolved && m2.isSolved {
+					switch monkey.operation {
+					case "+":
+						monkey.value = m1.value + m2.value
+					case "-":
+						monkey.value = m1.value - m2.value
+					case "*":
+						monkey.value = m1.value * m2.value
+					case "/":
+						monkey.value = m1.value / m2.value
+
+					default:
+						panic("Operation " + monkey.operation + " not supported.")
+
+					}
+
+					monkey.isSolved = true
+					index[key] = monkey
+					somethingSolved = true
+				}
+			}
+		}
+	}
+
+	// expectedNumber := 0
+	// var monkeyToFind Monkey
+
+	// if index[index["root"].m1].isSolved {
+	// 	expectedNumber = index[index["root"].m1].value
+	// 	monkeyToFind = index[index["root"].m2]
+	// } else {
+	// 	expectedNumber = index[index["root"].m2].value
+	// 	monkeyToFind = index[index["root"].m1]
+	// }
+
+	// fmt.Println("expected", expectedNumber, "pending monkey", monkeyToFind.id)
+	//operations := []func(n int) int{}
+
+	// currentMonkey := monkeyToFind
+	// for currentMonkey.id != "humn" {
+
+	// }
 
 	for !index3["root"] {
 		for key, monkey := range index {
@@ -103,10 +160,12 @@ func main() {
 					descriptions[key] = strconv.Itoa(monkey.value)
 				} else {
 					// The monkey needs other monkeys data
+
 					f1, ok1 := index2[monkey.m1]
 					f2, ok2 := index2[monkey.m2]
 
 					if ok1 && ok2 {
+
 						switch monkey.operation {
 						case "+":
 							index2[key] = func(f1, f2 func() int, key string) func() int {
@@ -124,13 +183,13 @@ func main() {
 							index2[key] = func(f1, f2 func() int, key string) func() int {
 								return func() int { return f1() * f2() }
 							}(f1, f2, key)
-							descriptions[key] = "(" + descriptions[monkey.m1] + "*" + descriptions[monkey.m2] + ")"
+							descriptions[key] = descriptions[monkey.m1] + "*" + descriptions[monkey.m2]
 
 						case "/":
 							index2[key] = func(f1, f2 func() int, key string) func() int {
 								return func() int { return f1() / f2() }
 							}(f1, f2, key)
-							descriptions[key] = "(" + descriptions[monkey.m1] + "/" + descriptions[monkey.m2] + ")"
+							descriptions[key] = descriptions[monkey.m1] + "/" + descriptions[monkey.m2]
 
 						case "=":
 							index2[key] = func(f1, f2 func() int) func() int {
@@ -142,18 +201,22 @@ func main() {
 
 						index3[key] = true
 					}
+
 				}
 			}
 		}
-
 	}
 
 	fmt.Println("\n\n\n\n\n")
 	f := index2["root"]
-	// currentHumnValue = -305
-	// fmt.Println(f())
 
-	// fmt.Println(descriptions["root"])
+	currentHumnValue = 3871012596468
+	fmt.Println(f())
+
+	currentHumnValue = 7628196411405
+	fmt.Println(f())
+
+	fmt.Println(descriptions["root"])
 
 	for currentHumnValue = 0; currentHumnValue < math.MaxInt; currentHumnValue++ {
 		if currentHumnValue%1000000 == 0 {
