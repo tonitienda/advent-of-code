052c21b367e2bf878aab0340b3f953a50e004b5c
Toni Tienda <tonitienda@gmail.com>
2022-12-22 20:40:25 +0900

    golang: 2022 - day 17 part1 (unfinished)

---
+++ golang/2022/day-16/puzzle-2.go
@@ -0,0 +1,246 @@
+package main
+
+import (
+	"advent/utils/array"
+	"advent/utils/funct"
+	"advent/utils/input"
+	"advent/utils/output"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+)
+
+func StrToInt(str string) int {
+	return funct.GetValue(strconv.Atoi(str))
+}
+
+type Valve struct {
+	id    string
+	rate  int
+	edges []Edge
+}
+
+type Edge struct {
+	destId string
+	cost   int
+}
+
+func (v Valve) String() string {
+	return fmt.Sprintf("%s (%d) -> %s", v.id, v.rate, v.edges)
+
+}
+
+func getValve(line string) Valve {
+	// Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+	cleanStr := strings.ReplaceAll(line, "Valve ", "")
+	cleanStr = strings.ReplaceAll(cleanStr, " has flow rate=", " ")
+
+	cleanStr = strings.ReplaceAll(cleanStr, "; tunnels lead to valves ", " ")
+	cleanStr = strings.ReplaceAll(cleanStr, "; tunnel leads to valve ", " ")
+
+	cleanStr = strings.ReplaceAll(cleanStr, ", ", ",")
+
+	parts := strings.Split(cleanStr, " ")
+
+	id := parts[0]
+	rate := parts[1]
+	connections := strings.Split(parts[2], ",")
+
+	return Valve{
+		id:    id,
+		rate:  StrToInt(rate),
+		edges: array.Map(connections, func(conn string) Edge { return Edge{destId: conn, cost: 1} }),
+	}
+}
+
+func calculateMinDistance(valve1, valve2 string, index map[string]Valve, cost int, path []string) (int, []string, bool) {
+
+	if valve1 == valve2 {
+		return cost, path, true
+	}
+
+	valve := index[valve1]
+
+	//fmt.Println(tabs(len(path)), valve1, "=>", valve2, "children", valve.children, ":", path)
+
+	edges := array.Map(valve.edges, func(e Edge) string { return e.destId })
+	if includes(edges, valve2) {
+		return cost + 1, append(path, valve2), true
+	}
+
+	minCost := int(math.Inf(1))
+	minPath := []string{}
+
+	atLeastOnePathFound := false
+	for _, edge := range valve.edges {
+
+		// Avoid cycles
+		if includes(path, edge.destId) {
+			continue
+		}
+
+		cost, newPath, ok := calculateMinDistance(edge.destId, valve2, index, cost+1, append(path, edge.destId))
+		atLeastOnePathFound = atLeastOnePathFound || ok
+
+		if ok {
+			if cost < minCost {
+				minCost = cost
+				minPath = newPath
+			}
+		}
+	}
+
+	return minCost, minPath, atLeastOnePathFound
+}
+
+func makeValveMap(valves []Valve) map[string]Valve {
+	index := map[string]Valve{}
+
+	for _, node := range valves {
+		index[node.id] = node
+	}
+
+	return index
+}
+
+func includes(arr []string, val string) bool {
+	for _, v := range arr {
+		if v == val {
+			return true
+		}
+	}
+
+	return false
+}
+
+func allVisited(index map[string]Valve, visited map[string]bool) bool {
+	for key, _ := range index {
+		if !visited[key] {
+			return false
+		}
+	}
+
+	return true
+}
+
+var iterations int = 0
+
+func bruteForce(origin string, index map[string]Valve, availableMinutes int, accPressure int, visited map[string]bool) int {
+	iterations++
+	if availableMinutes <= 0 || allVisited(index, visited) {
+		return accPressure
+	}
+
+	valve := index[origin]
+	edges := valve.edges
+
+	//fmt.Println("From", origin, "walking", len(reachableNodes), "nodes")
+
+	maxPressure := accPressure
+
+	for _, edge := range edges {
+		if !visited[edge.destId] {
+
+			// 1 minute for each node we visit to read destination
+			minutesSpent := edge.cost
+			pressureAcc := 0
+
+			destNode := index[edge.destId]
+
+			// This should always be true, since we removed the nodes that do not have value
+			if destNode.rate > 0 {
+				// 1m Opening the valve
+				minutesSpent++
+				pressureAcc = destNode.rate * (availableMinutes - minutesSpent)
+			}
+
+			if availableMinutes-minutesSpent <= 0 {
+				continue
+			}
+
+			visited[destNode.id] = true
+			pressure := bruteForce(destNode.id, index, availableMinutes-minutesSpent, accPressure+pressureAcc, visited)
+			visited[destNode.id] = false
+
+			if pressure > maxPressure {
+				maxPressure = pressure
+			}
+		}
+
+	}
+
+	//fmt.Println("Current max:", maxPressure, bestPath)
+	return maxPressure
+}
+
+func getMarkDown(valves []Valve, index map[string]Valve) string {
+	contents := "```mermaid\nflowchart LR\n"
+
+	printed := map[string]bool{}
+	for _, valve := range valves {
+		for _, edge := range valve.edges {
+			reverseEdgeId := edge.destId + "_" + valve.id
+			edgeId := valve.id + "_" + edge.destId
+
+			if !printed[reverseEdgeId] {
+				contents += valve.id + " -- " + strconv.Itoa(edge.cost) + " --- " + edge.destId + "\n"
+				printed[edgeId] = true
+			}
+		}
+	}
+
+	contents += "\n```\n\n"
+
+	contents += "Total nodes: " + strconv.Itoa(len(valves)) + ".\n"
+
+	return contents
+}
+
+func main() {
+
+	execType := "input"
+	data := input.GetLines(2022, 16, execType+".txt")
+
+	valves := array.Map(data, getValve)
+
+	index := makeValveMap(valves)
+
+	fmt.Println(index)
+
+	valves2 := []Valve{}
+
+	// We only care about destinations that bring value
+	for _, valve1 := range valves {
+		if valve1.rate > 0 || valve1.id == "AA" {
+			valve := Valve{id: valve1.id, rate: valve1.rate, edges: []Edge{}}
+			for _, valve2 := range valves {
+				if valve1.id != valve2.id && valve2.rate > 0 {
+					_, minPath, _ := calculateMinDistance(valve1.id, valve2.id, index, 0, []string{})
+					valve.edges = append(valve.edges, Edge{destId: valve2.id, cost: len(minPath)})
+				}
+
+			}
+			valves2 = append(valves2, valve)
+		}
+	}
+
+	originalValves := getMarkDown(valves, index)
+	calculatedValves := getMarkDown(valves2, index)
+
+	document := "### Original\n\n" + originalValves + "\n\n### New\n\n" + calculatedValves
+
+	output.WriteContents(2022, 16, execType+".md", document)
+
+	visited := map[string]bool{}
+	visited["AA"] = true
+
+	index2 := makeValveMap(valves2)
+
+	fmt.Println("---")
+	result := bruteForce("AA", index2, 30, 0, visited)
+	fmt.Println("---")
+
+	fmt.Println(result, "(", iterations, ")")
+
+}
---
+++ golang/2022/day-17/puzzle-1.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"advent/utils/funct"
+	"advent/utils/input"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// Shapes start in the column #2
+
+func BinaryToInt(str string) int {
+	return int(funct.GetValue(strconv.ParseInt(str, 2, 32)))
+}
+
+type Shape struct {
+	structure []int
+	width     int
+	height    int
+	top       int
+	left      int
+}
+
+var HLine = Shape{
+	structure: []int{BinaryToInt("0011110")},
+	width:     4,
+	height:    1,
+}
+
+// 0001000 => 8
+// 0011100 => 24
+// 0001000 =>
+var Plus = Shape{
+	structure: []int{
+		BinaryToInt("0001000"),
+		BinaryToInt("0011100"),
+		BinaryToInt("0001000"),
+	},
+	width:  3,
+	height: 3,
+}
+
+var LShape = Shape{
+	structure: []int{
+		BinaryToInt("0000100"),
+		BinaryToInt("0000100"),
+		BinaryToInt("0011100"),
+	},
+	width:  3,
+	height: 3,
+}
+
+var VLine = Shape{
+	structure: []int{
+		BinaryToInt("0010000"),
+		BinaryToInt("0010000"),
+		BinaryToInt("0010000"),
+		BinaryToInt("0010000"),
+	},
+	width:  1,
+	height: 4,
+}
+
+var Square = Shape{
+	structure: []int{
+		BinaryToInt("0011000"),
+		BinaryToInt("0011000"),
+	},
+	width:  2,
+	height: 2,
+}
+
+var Shapes = []Shape{HLine, Plus, LShape, VLine, Square}
+
+func printBoard(board []int) {
+	for i := len(board) - 1; i >= 0; i-- {
+		binary := fmt.Sprintf("%07s", strconv.FormatInt(int64(board[i]), 2))
+		fmt.Println(strings.ReplaceAll(strings.ReplaceAll(binary, "0", "."), "1", "#"))
+	}
+
+}
+
+// TODO - For now we ignore the pieces moving
+func findCollission(shape []int, board []int) int {
+	for boardRow := len(board) - 1; boardRow >= len(shape)-1; boardRow-- {
+		for i := 0; i < len(shape); i++ {
+			if board[boardRow-i]&shape[i] > 0 {
+				// Collission, so we return the previous row
+				return boardRow + 1
+			}
+		}
+	}
+	return len(shape) - 1
+}
+
+func main() {
+
+	execType := "test"
+	data := strings.Split(input.GetContents(2022, 17, execType+".txt"), "")
+
+	fmt.Println(data)
+
+	board := []int{}
+	shapeCount := 0
+	lastRowWithFallenRocks := 0
+
+	for {
+		// Falling means going from the end of the board to the beginning
+
+		currentShape := Shapes[shapeCount%5]
+		freeRows := len(board) - lastRowWithFallenRocks
+		requiredFreeRows := 3 + len(currentShape.structure)
+
+		fmt.Println("requiredFreeRows", requiredFreeRows, "freeRows", freeRows)
+		for i := 0; i < requiredFreeRows-freeRows; i++ {
+			board = append(board, 0)
+		}
+
+		printBoard(board)
+
+		row := findCollission(currentShape.structure, board)
+		lastRowWithFallenRocks = row + len(currentShape.structure) - 1
+		fmt.Println("Collission at:", row)
+		for i := 0; i < len(currentShape.structure); i++ {
+			board[row-i] |= currentShape.structure[i]
+		}
+
+		printBoard(board)
+		fmt.Println()
+		shapeCount++
+
+		if shapeCount > 2 {
+			return
+		}
+	}
+
+}
