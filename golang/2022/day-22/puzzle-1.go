a22904fdc55b1be6eb961a19fc19f00f61c78b86
Toni Tienda <tonitienda@gmail.com>
2022-12-28 07:31:13 +0900

    golang: 2022 - day 22  part1

--- golang/2022/day-22/puzzle-1.go
+++ golang/2022/day-22/puzzle-1.go
@@ -22,6 +22,7 @@ type Movement struct {
 var Path = 0
 var Wall = 1
 var Nothing = -1
+var Me = 99
 
 var TurnRight = -1
 var TurnLeft = -2
@@ -44,63 +45,66 @@ func getValue(str string) int {
 	}
 }
 
-func findLastCellByRow(row int, board [][]int) (Point, bool) {
-	for col := len(board[row]) - 1; col <= 0; col-- {
-		if board[row][col] == Wall {
-			return Point{row: row, col: col}, false
-		}
-		if board[row][col] == Path {
-			return Point{row: row, col: col}, true
+func getSymbol(val int) string {
+	switch val {
+	case Path:
+		return "."
+	case Wall:
+		return "#"
+	case Nothing:
+		return " "
+	case Me:
+		return "@"
+	default:
+		panic("Value not supported:" + strconv.Itoa(val))
+
+	}
+}
+
+func findLastCellByRow(row int, board [][]int) Point {
+	for col := len(board[row]) - 1; col >= 0; col-- {
+		if board[row][col] != Nothing {
+			return Point{row: row, col: col}
 		}
 	}
 
 	panic("")
 }
 
-func findLastCellByCol(col int, board [][]int) (Point, bool) {
-	for row := len(board) - 1; row <= 0; row-- {
-		if board[row][col] == Wall {
-			return Point{row: row, col: col}, false
+func findLastCellByCol(col int, board [][]int) Point {
+	for row := len(board) - 1; row >= 0; row-- {
+		if col >= len(board[row]) {
+			continue
 		}
-		if board[row][col] == Path {
-			return Point{row: row, col: col}, true
+		if board[row][col] != Nothing {
+			return Point{row: row, col: col}
 		}
 	}
 
-	panic("")
+	panic("...")
 }
 
-func findFirstCellByRow(row int, board [][]int) (Point, bool) {
-	for col, val := range board[row] {
-		if val == Wall {
-			return Point{row: row, col: col}, false
-		}
-		if val == Path {
-			return Point{row: row, col: col}, true
+func findFirstCellByRow(row int, board [][]int) Point {
+	for col, _ := range board[row] {
+		if board[row][col] != Nothing {
+			return Point{row: row, col: col}
 		}
 	}
 
 	panic("First path could not be found in row " + strconv.Itoa(row))
 }
 
-func findFirstCellByCol(col int, board [][]int) (Point, bool) {
+func findFirstCellByCol(col int, board [][]int) Point {
 	for row, _ := range board {
-		if board[row][col] == Wall {
-			return Point{row: row, col: col}, false
+		if col >= len(board[row]) {
+			continue
 		}
-		if board[row][col] == Path {
-			return Point{row: row, col: col}, true
+		if board[row][col] != Nothing {
+			return Point{row: row, col: col}
 		}
 	}
 
 	panic("First path could not be found in col " + strconv.Itoa(col))
-}
-
-var Movements = map[string]Movement{
-	"right": {row: 0, col: 1},
-	"left":  {row: 0, col: -1},
-	"up":    {row: -1, col: 0},
-	"down":  {row: 1, col: 0},
 }
 
 var FinalValues = map[string]int{
@@ -162,73 +166,94 @@ func processCommandLine(commandline string) []int {
 
 		}
 	}
+
+	if current != "" {
+		commands = append(commands, StrToInt(current))
+	}
 	return commands
 }
 
-func move(p Point, direction string, board [][]int) Point {
+func moveUp(p Point, board [][]int) Point {
+	next := Point{row: p.row - 1, col: p.col}
 
-	m := Movements[direction]
-	next := Point{
-		row: p.row + m.row,
-		col: p.col + m.col,
+	// If next point is out of limits or not walkable, try to wrap to the other side
+	if next.row < 0 || board[next.row][next.col] == Nothing {
+		next = findLastCellByCol(next.col, board)
 	}
 
-	if next.row < 0 {
-		next, _ = findLastCellByCol(next.col, board)
-	} else if next.col < 0 {
-		next, _ = findLastCellByRow(next.row, board)
-
-	} else if next.row >= len(board) {
-		next, _ = findFirstCellByCol(next.col, board)
-
-	} else if next.col >= len(board[next.row]) {
-		next, _ = findFirstCellByRow(next.row, board)
-
-	}
-
-	fmt.Println("current", p, "next", next)
-	if board[next.row][next.col] == Nothing {
-		switch direction {
-		case "right":
-			next2, ok := findFirstCellByRow(next.row, board)
-			if ok {
-				next = next2
-			} else {
-				return p
-			}
-
-		case "down":
-			next2, ok := findFirstCellByCol(next.col, board)
-			if ok {
-				next = next2
-			} else {
-				return p
-			}
-		case "left":
-			next2, ok := findLastCellByRow(next.row, board)
-			if ok {
-				next = next2
-			} else {
-				return p
-			}
-
-		case "up":
-			next2, ok := findLastCellByCol(next.col, board)
-			if ok {
-				next = next2
-			} else {
-				return p
-			}
-
-		}
-	}
-
+	// If the next point is a wall, return the current point
 	if board[next.row][next.col] == Wall {
-		fmt.Println("Next cell is a wall. Returning", p)
 		return p
 	}
-	fmt.Println("\tReturning", next)
+
 	return next
+
+}
+
+func moveDown(p Point, board [][]int) Point {
+	next := Point{row: p.row + 1, col: p.col}
+
+	// If next point is out of limits or not walkable, try to wrap to the other side
+	if next.row >= len(board) || next.col >= len(board[next.row]) || board[next.row][next.col] == Nothing {
+		next = findFirstCellByCol(next.col, board)
+	}
+
+	// If the next point is a wall, return the current point
+	if board[next.row][next.col] == Wall {
+		return p
+	}
+
+	return next
+
+}
+
+func moveRight(p Point, board [][]int) Point {
+	next := Point{row: p.row, col: p.col + 1}
+
+	// If next point is out of limits or not walkable, try to wrap to the other side
+	if next.col >= len(board[next.row]) || board[next.row][next.col] == Nothing {
+		next = findFirstCellByRow(next.row, board)
+	}
+
+	// If the next point is a wall, return the current point
+	if board[next.row][next.col] == Wall {
+		return p
+	}
+
+	return next
+
+}
+
+func moveLeft(p Point, board [][]int) Point {
+	next := Point{row: p.row, col: p.col - 1}
+
+	// If next point is out of limits or not walkable, try to wrap to the other side
+	if next.col < 0 || board[next.row][next.col] == Nothing {
+		next = findLastCellByRow(next.row, board)
+	}
+
+	// If the next point is a wall, return the current point
+	if board[next.row][next.col] == Wall {
+		return p
+	}
+
+	return next
+
+}
+
+func move(p Point, direction string, board [][]int) Point {
+	switch direction {
+	case "up":
+		return moveUp(p, board)
+	case "right":
+		return moveRight(p, board)
+	case "down":
+		return moveDown(p, board)
+	case "left":
+		return moveLeft(p, board)
+	}
+
+	panic("Direction not supported")
 
 }
 
@@ -242,12 +267,16 @@ var directionSymbol = map[string]string{
 func printBoard(board [][]int, directions map[Point]string) {
 	for row, _ := range board {
 		for col, _ := range board[row] {
+			if board[row][col] == Me {
+				fmt.Print("@")
+				continue
+			}
+
 			direction, ok := directions[Point{row: row, col: col}]
 
 			if ok {
 				fmt.Print(directionSymbol[direction])
 			} else {
-
 				switch board[row][col] {
 				case Wall:
 					fmt.Print("#")
@@ -263,47 +292,41 @@ func printBoard(board [][]int, directions map[Point]string) {
 }
 
 func main() {
-	execType := "test"
+	execType := "input"
 
 	data := strings.Split(input.GetContents(2022, 22, execType+".txt"), "\n\n")
 
 	boardDesc := data[0]
 	commandline := data[1]
+	//commandline = "10R1L2R16R6R13R6L41R41R9L3R33L39R30"
 
 	board := array.Map(strings.Split(boardDesc, "\n"), func(str string) []int { return array.Map(strings.Split(str, ""), getValue) })
 	commands := processCommandLine(commandline)
 
-	fmt.Println(board)
-	fmt.Println(commands)
+	//fmt.Println(commands)
 
-	current, _ := findFirstCellByRow(0, board)
+	current := findFirstCellByRow(0, board)
 	direction := "right"
 
 	trail := map[Point]string{}
-	printBoard(board, trail)
 	for _, command := range commands {
 
 		trail[current] = direction
 
-		fmt.Println(current)
-		fmt.Println("command", command)
 		if command == TurnLeft || command == TurnRight {
 			direction = changeMovement(direction, command)
-			fmt.Println("direction", direction)
-
 		} else {
-			fmt.Println("moving", command, "units")
-
 			for i := 0; i < command; i++ {
 				// We can optimize this. If there is a wall we can skip some iterations
 				current = move(current, direction, board)
 				trail[current] = direction
 			}
 		}
-		fmt.Println()
+		//fmt.Println()
 	}
 
-	printBoard(board, trail)
+	//board[current.row][current.col] = Me
+	//printBoard(board, trail)
 
 	// Expected (rows index 1) row 6, col 8, direction 0 (right) 1000 * 6 + 4 * 8 + 0: 6032
 	fmt.Println(current, direction)
